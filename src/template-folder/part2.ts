74789e2827aef3db78da0647460990c7d1b37fc1
mabenj <markobenj@hotmail.com>
2024-12-01 21:53:59 +0200

    Add template folder

---
+++ src/template-folder/part2.ts
@@ -0,0 +1,7 @@
+import { PuzzleSolver } from "../puzzle-solver";
+
+export class DayXPart2 implements PuzzleSolver {
+  solve(input: string): string {
+    throw new Error("Method not implemented.");
+  }
+}
