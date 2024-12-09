9a3378ede84b53c2941b7e005cf04c0003c2f029
mabenj <markobenj@hotmail.com>
2024-12-10 01:35:48 +0200

    Update readme script

--- lib/scripts/generate-readme.ts
+++ lib/scripts/generate-readme.ts
@@ -116,8 +116,9 @@ async function generateReadme(puzzles: PuzzleData[]) {
         }
 
         result.push(
-            `> Generated at ${new Date().toUTCString()} [![Generate README](https://github.com/mabenj/aoc-deno-ts/actions/workflows/generate-readme.yml/badge.svg)](https://github.com/mabenj/aoc-deno-ts/actions/workflows/generate-readme.yml)`
+            "[![Generate README](https://github.com/mabenj/aoc-deno-ts/actions/workflows/generate-readme.yml/badge.svg)](https://github.com/mabenj/aoc-deno-ts/actions/workflows/generate-readme.yml)"
         );
+        result.push(`> Generated at ${new Date().toUTCString()}`);
         result.push("");
 
         return result;
