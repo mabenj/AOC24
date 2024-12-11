51198530b7addfd4a9ab714be6874827715b524a
mabenj <markobenj@hotmail.com>
2024-12-12 00:31:53 +0200

    Further optimize and clean day 11 solution

--- lib/24D11/solver.ts
+++ lib/24D11/solver.ts
@@ -1,20 +1,7 @@
-import { memoize } from "../common.ts";
 import { PuzzleSolver } from "../types/puzzle-solver.ts";
 
 export default class Solver24D11 implements PuzzleSolver {
     private initialStones: number[] = [];
-    private handleStone = memoize((stoneNumber: number) => {
-        if (stoneNumber === 0) {
-            return [1];
-        }
-        if (hasEvenDigits(stoneNumber)) {
-            const str = stoneNumber.toString();
-            const left = Number(str.slice(0, str.length / 2));
-            const right = Number(str.slice(str.length / 2));
-            return [left, right];
-        }
-        return [stoneNumber * 2024];
-    });
 
     parseInput(input: string[]) {
         this.initialStones = input[0].split(" ").map(Number);
@@ -29,26 +16,34 @@ export default class Solver24D11 implements PuzzleSolver {
     }
 
     private blink(n: number) {
-        let sum = 0;
-        for (const stone of this.initialStones) {
-            const frequencyMap = new Map<number, number>([[stone, 1]]);
-            for (let i = 0; i < n; i++) {
-                for (const [stone, count] of [...frequencyMap]) {
-                    alterMapValue(frequencyMap, stone, -count);
-                    const resultStones = this.handleStone(stone);
-                    resultStones.forEach((stone) =>
-                        alterMapValue(frequencyMap, stone, count)
-                    );
-                }
+        const frequencyMap = new Map<number, number>();
+        this.initialStones.forEach((stone) =>
+            alterMapValue(frequencyMap, stone, 1)
+        );
+        for (let i = 0; i < n; i++) {
+            for (const [stone, count] of [...frequencyMap]) {
+                alterMapValue(frequencyMap, stone, -count);
+                const resultStones = handleStone(stone);
+                resultStones.forEach((stone) =>
+                    alterMapValue(frequencyMap, stone, count)
+                );
             }
-            sum += frequencyMap.values().reduce((acc, count) => acc + count, 0);
         }
-        return sum;
+        return frequencyMap.values().reduce((acc, count) => acc + count, 0);
     }
 }
 
-function hasEvenDigits(num: number) {
-    return num.toString().length % 2 === 0;
+function handleStone(stoneNumber: number) {
+    if (stoneNumber === 0) {
+        return [1];
+    }
+    const str = stoneNumber.toString();
+    if (str.length % 2 === 0) {
+        const left = Number(str.slice(0, str.length / 2));
+        const right = Number(str.slice(str.length / 2));
+        return [left, right];
+    }
+    return [stoneNumber * 2024];
 }
 
 function alterMapValue(
@@ -56,8 +51,10 @@ function alterMapValue(
     key: number,
     valueToAdd: number
 ) {
-    if (valueToAdd === 0) {
-        return;
+    const newValue = (map.get(key) ?? 0) + valueToAdd;
+    if (newValue === 0) {
+        map.delete(key);
+    } else {
+        map.set(key, newValue);
     }
-    map.set(key, (map.get(key) ?? 0) + valueToAdd);
 }
