5c0a69c723425746aeb4acca13f4e95efc2b9e03
mabenj <markobenj@hotmail.com>
2024-12-17 20:54:19 +0200

    Refactor and tidy up day 17 part 1

--- lib/24D17/solver.ts
+++ lib/24D17/solver.ts
@@ -6,149 +6,76 @@ Register C: 0
 
 Program: 0,1,5,4,3,0
 `;
+// const EXAMPLE = `Register A: 2024
+// Register B: 0
+// Register C: 0
+
+// Program: 0,3,5,4,3,0`;
 
 export default class Solver24D17 implements PuzzleSolver {
-    private computer!: Computer;
+    private registerA = 0;
+    private program: number[] = [];
 
     parseInput(input: string[]) {
-        input = EXAMPLE.split("\n");
-        this.computer = new Computer(
-            +input[0].split(": ")[1],
-            input[4].split(": ")[1].split(",").map(Number)
-        );
+        // input = EXAMPLE.split("\n");
+        this.registerA = +input[0].split(": ")[1];
+        this.program = input[4].split(": ")[1].split(",").map(Number);
     }
 
     solvePart1() {
-        const output = this.computer.run();
-        return output;
-    }
-
-    solvePart2(): number | string {
-        throw new Error("Method not implemented.");
-    }
-}
-
-class Computer {
-    private readonly instructionMap: Record<
-        number,
-        (operand: number) => number | void
-    > = {
-        0: this.handleAdv.bind(this),
-        1: this.handleBxl.bind(this),
-        2: this.handleBst.bind(this),
-        3: this.handleJnz.bind(this),
-        4: this.handleBxc.bind(this),
-        5: this.handleOut.bind(this),
-        6: this.handleBdv.bind(this),
-        7: this.handleCdv.bind(this),
-    };
-    private readonly program: number[];
-    private readonly registers: Record<string, number> = {
-        A: 0,
-        B: 0,
-        C: 0,
-    };
-    private ip = 0;
-
-    constructor(registerA: number, program: number[]) {
-        this.program = program;
-        this.writeRegister("A", registerA);
-        this.writeRegister("B", 0);
-        this.writeRegister("C", 0);
-    }
-
-    run() {
+        const registers = { A: this.registerA, B: 0, C: 0 };
         const output: number[] = [];
-        while (this.ip + 1 < this.program.length) {
-            const instruction = this.program[this.ip];
-            const operand = this.program[this.ip + 1];
-            const result = this.instructionMap[instruction](operand);
-            if (typeof result === "number") {
-                output.push(result);
+
+        const combo = (literal: number) => {
+            if (literal <= 3) return literal;
+            if (literal === 4) return registers["A"];
+            if (literal === 5) return registers["B"];
+            if (literal === 6) return registers["C"];
+            throw new Error(`Invalid operand ${literal}`);
+        };
+
+        let ip = 0;
+        while (ip < this.program.length) {
+            const opCode = this.program[ip++];
+            const operand = this.program[ip++];
+            switch (opCode) {
+                case 0:
+                    registers["A"] = Math.floor(
+                        registers["A"] / Math.pow(2, combo(operand))
+                    );
+                    break;
+                case 1:
+                    registers["B"] ^= operand;
+                    break;
+                case 2:
+                    registers["B"] = combo(operand) % 8;
+                    break;
+                case 3:
+                    ip = registers["A"] === 0 ? ip : operand;
+                    break;
+                case 4:
+                    registers["B"] ^= registers["C"];
+                    break;
+                case 5:
+                    output.push(combo(operand) % 8);
+                    break;
+                case 6:
+                    registers["B"] = Math.floor(
+                        registers["A"] / Math.pow(2, combo(operand))
+                    );
+                    break;
+                case 7:
+                    registers["C"] = Math.floor(
+                        registers["A"] / Math.pow(2, combo(operand))
+                    );
+                    break;
             }
-            this.ip += 2;
         }
+
         return output.join(",");
     }
 
-    private handleAdv(operand: number) {
-        const numerator = this.readRegister("A");
-        const denominator = Math.pow(2, this.getComboOperandValue(operand));
-        this.writeRegister("A", Math.floor(numerator / denominator));
-    }
-
-    private handleBxl(operand: number) {
-        this.writeRegister("B", this.readRegister("B") ^ operand);
-    }
-
-    private handleBst(operand: number) {
-        this.writeRegister("B", this.getComboOperandValue(operand) % 8);
-    }
-
-    private handleJnz(operand: number) {
-        const valueA = this.readRegister("A");
-        if (valueA === 0) {
-            return;
-        }
-        this.ip = operand - 2; // -2 negates the increment
-    }
-
-    private handleBxc(_: number) {
-        this.writeRegister(
-            "B",
-            this.readRegister("B") ^ this.readRegister("C")
-        );
-    }
-
-    private handleOut(operand: number) {
-        return this.getComboOperandValue(operand) % 8;
-    }
-
-    private handleBdv(operand: number) {
-        const numerator = this.readRegister("A");
-        const denominator = Math.pow(2, this.getComboOperandValue(operand));
-        this.writeRegister("B", Math.floor(numerator / denominator));
-    }
-
-    private handleCdv(operand: number) {
-        const numerator = this.readRegister("A");
-        const denominator = Math.pow(2, this.getComboOperandValue(operand));
-        this.writeRegister("C", Math.floor(numerator / denominator));
-    }
-
-    private getComboOperandValue(operand: number) {
-        switch (operand) {
-            case 0:
-            case 1:
-            case 2:
-            case 3:
-                return operand;
-            case 4:
-                return this.readRegister("A");
-            case 5:
-                return this.readRegister("B");
-            case 6:
-                return this.readRegister("C");
-            default:
-                throw new Error(`Invalid operand: ${operand}`);
-        }
-    }
-
-    private writeRegister(register: string, value: number) {
-        console.log(
-            `WRITE %c${register} = %c${value}`,
-            "color: gray",
-            "color: white"
-        );
-        this.registers[register] = value;
-    }
-
-    private readRegister(register: string): number {
-        console.log(
-            `READ %c${register} = %c${this.registers[register]}`,
-            "color: gray",
-            "color: white"
-        );
-        return this.registers[register];
+    solvePart2() {
+        return -1;
     }
 }
