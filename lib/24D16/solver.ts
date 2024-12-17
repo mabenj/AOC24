0624536ff031fca554ac4593fbeb3c832b527153
Marko Järvinen <markobenj@hotmail.com>
2024-12-17 11:59:28 +0200

    Solve day 16 part 2

--- lib/24D16/solver.ts
+++ lib/24D16/solver.ts
@@ -1,140 +1,162 @@
 import { objectKeys } from "../common.ts";
 import { PuzzleSolver } from "../types/puzzle-solver.ts";
 
-const EXAMPLE = `#################
-#...#...#...#..E#
-#.#.#.#.#.#.#.#.#
-#.#.#.#...#...#.#
-#.#.#.#.###.#.#.#
-#...#.#.#.....#.#
-#.#.#.#.#.#####.#
-#.#...#.#.#.....#
-#.#.#####.#.###.#
-#.#.#.......#...#
-#.#.###.#####.###
-#.#.#...#.....#.#
-#.#.#.#####.###.#
-#.#.#.........#.#
-#.#.#.#########.#
-#S#.............#
-#################`;
-
 type Graph = { [from: Node]: { [to: Node]: number } };
 type Node = `${number},${number},${Direction}`;
 type Direction = "N" | "S" | "W" | "E";
+type PathNode = {
+    node: Node;
+    prev: PathNode[] | null;
+};
 
 export default class Solver24D16 implements PuzzleSolver {
     private graph: Graph = {};
     private startNode: Node = "-1,-1,E";
-    private endNodes = new Set<Node>();
+    private endNodes: Node[] = [];
 
     parseInput(input: string[]) {
-        // input = EXAMPLE.split("\n");
         const grid = input.filter((line) => !!line).map((row) => row.split(""));
+
+        const setGraph = (from: Node, to: Node, weight: number) => {
+            this.graph[from] ??= {};
+            this.graph[from][to] = weight;
+        };
 
         for (let y = 0; y < grid.length; y++) {
             for (let x = 0; x < grid[y].length; x++) {
                 if (grid[y][x] === "#") {
                     continue;
                 }
+
+                const canGoNorth = grid[y - 1][x] !== "#";
+                const canGoSouth = grid[y + 1][x] !== "#";
+                const canGoWest = grid[y][x - 1] !== "#";
+                const canGoEast = grid[y][x + 1] !== "#";
+
                 if (grid[y][x] === "S") {
                     this.startNode = `${x},${y},E`;
                 }
                 if (grid[y][x] === "E") {
-                    this.endNodes = new Set([
-                        `${x},${y},N`,
-                        `${x},${y},S`,
-                        `${x},${y},W`,
-                        `${x},${y},E`,
-                    ] as Node[]);
+                    if (canGoNorth) this.endNodes.push(`${x},${y},S`);
+                    if (canGoSouth) this.endNodes.push(`${x},${y},N`);
+                    if (canGoWest) this.endNodes.push(`${x},${y},E`);
+                    if (canGoEast) this.endNodes.push(`${x},${y},W`);
                 }
-                this.setRotationsToGraph(x, y);
-                this.setMovementsToGraph(grid, x, y);
+
+                if (canGoNorth) {
+                    setGraph(`${x},${y},N`, `${x},${y - 1},N`, 1);
+                    setGraph(`${x},${y},E`, `${x},${y},N`, 1000);
+                    setGraph(`${x},${y},W`, `${x},${y},N`, 1000);
+                }
+                if (canGoSouth) {
+                    setGraph(`${x},${y},S`, `${x},${y + 1},S`, 1);
+                    setGraph(`${x},${y},E`, `${x},${y},S`, 1000);
+                    setGraph(`${x},${y},W`, `${x},${y},S`, 1000);
+                }
+                if (canGoWest) {
+                    setGraph(`${x},${y},W`, `${x - 1},${y},W`, 1);
+                    setGraph(`${x},${y},N`, `${x},${y},W`, 1000);
+                    setGraph(`${x},${y},S`, `${x},${y},W`, 1000);
+                }
+                if (canGoEast) {
+                    setGraph(`${x},${y},E`, `${x + 1},${y},E`, 1);
+                    setGraph(`${x},${y},N`, `${x},${y},E`, 1000);
+                    setGraph(`${x},${y},S`, `${x},${y},E`, 1000);
+                }
             }
         }
     }
 
     solvePart1() {
-        // dijkstra
-        const distances: { [node: Node]: number } = {};
-        const prev: { [node: Node]: Node | null } = {};
-        const priorityQueue: { node: Node; distance: number }[] = [];
+        const { distance } = dijkstra(
+            this.graph,
+            this.startNode,
+            this.endNodes,
+            false
+        );
+        return distance;
+    }
 
-        objectKeys(this.graph).forEach((node) => {
-            distances[node] = Infinity;
-            prev[node] = null;
-        });
-        distances[this.startNode] = 0;
-
-        priorityQueue.push({ node: this.startNode, distance: 0 });
-
-        while (priorityQueue.length > 0) {
-            priorityQueue.sort((a, b) => a.distance - b.distance);
-            const { node: u } = priorityQueue.shift()!;
-            if (this.endNodes.has(u)) {
-                return distances[u];
+    solvePart2() {
+        const { paths } = dijkstra(
+            this.graph,
+            this.startNode,
+            this.endNodes,
+            true
+        );
+        const uniqueSeats = new Set<string>();
+        for (const path of paths) {
+            for (const node of path) {
+                const [x, y] = node.split(",");
+                uniqueSeats.add(`${x},${y}`);
             }
+        }
+        return uniqueSeats.size;
+    }
+}
 
-            objectKeys(this.graph[u]).forEach((v) => {
-                const alt = distances[u] + this.graph[u][v];
+function dijkstra(
+    graph: Graph,
+    startNode: Node,
+    endNodes: Node[],
+    getPaths: boolean
+) {
+    const distances: { [node: Node]: number } = {};
+    const paths: { [node: Node]: PathNode } = {};
+    const priorityQueue: { node: Node; distance: number }[] = [];
 
-                if (alt < distances[v]) {
-                    distances[v] = alt;
-                    prev[v] = u;
-                    priorityQueue.push({ node: v, distance: alt });
-                }
+    objectKeys(graph).forEach((node) => {
+        distances[node] = Infinity;
+    });
+    distances[startNode] = 0;
+    paths[startNode] = { node: startNode, prev: null };
+
+    priorityQueue.push({ node: startNode, distance: 0 });
+
+    while (priorityQueue.length > 0) {
+        priorityQueue.sort((a, b) => a.distance - b.distance);
+        const { node: u } = priorityQueue.shift()!;
+
+        const neighbors = objectKeys(graph[u]);
+        for (const v of neighbors) {
+            const alt = distances[u] + graph[u][v];
+
+            if (alt < distances[v]) {
+                distances[v] = alt;
+                paths[v] = { node: v, prev: [paths[u]] };
+                priorityQueue.push({ node: v, distance: alt });
+            } else if (alt === distances[v]) {
+                paths[v].prev!.push(paths[u]);
+            }
+        }
+    }
+
+    const minDistance = Math.min(...endNodes.map((node) => distances[node]));
+    const bestPaths: Array<Node[]> = [];
+    if (getPaths) {
+        endNodes.forEach((node) => {
+            if (distances[node] !== minDistance) {
+                return;
+            }
+            constructPath(paths[node], []).forEach((path) => {
+                bestPaths.push(path);
             });
-        }
+        });
+    }
+    return {
+        distance: minDistance,
+        paths: bestPaths,
+    };
+}
 
-        throw new Error("No path found");
+function constructPath(lastNode: PathNode, pathSoFar: Node[]): Node[][] {
+    if (!lastNode.prev) {
+        return [[lastNode.node, ...pathSoFar]];
     }
 
-    solvePart2(): number | string {
-        throw new Error("Method not implemented.");
+    const paths: Node[][] = [];
+    for (const prevNode of lastNode.prev) {
+        paths.push(...constructPath(prevNode, [lastNode.node, ...pathSoFar]));
     }
-
-    private setRotationsToGraph(x: number, y: number) {
-        const ROTATIONS = [
-            ["N", "E"],
-            ["E", "S"],
-            ["S", "W"],
-            ["W", "N"],
-        ];
-        for (const [a, b] of ROTATIONS) {
-            this.setGraph(
-                `${x},${y},${a}` as Node,
-                `${x},${y},${b}` as Node,
-                1000
-            );
-            this.setGraph(
-                `${x},${y},${b}` as Node,
-                `${x},${y},${a}` as Node,
-                1000
-            );
-        }
-    }
-
-    private setMovementsToGraph(grid: string[][], x: number, y: number) {
-        if (grid[y - 1][x] !== "#") {
-            this.setGraph(`${x},${y},N`, `${x},${y - 1},N`, 1);
-            this.setGraph(`${x},${y - 1},S`, `${x},${y},S`, 1);
-        }
-        if (grid[y + 1][x] !== "#") {
-            this.setGraph(`${x},${y},S`, `${x},${y + 1},S`, 1);
-            this.setGraph(`${x},${y + 1},N`, `${x},${y},N`, 1);
-        }
-        if (grid[y][x - 1] !== "#") {
-            this.setGraph(`${x},${y},W`, `${x - 1},${y},W`, 1);
-            this.setGraph(`${x - 1},${y},E`, `${x},${y},E`, 1);
-        }
-        if (grid[y][x + 1] !== "#") {
-            this.setGraph(`${x},${y},E`, `${x + 1},${y},E`, 1);
-            this.setGraph(`${x + 1},${y},W`, `${x},${y},W`, 1);
-        }
-    }
-
-    private setGraph(from: Node, to: Node, weight: number) {
-        this.graph[from] ??= {};
-        this.graph[from][to] = weight;
-    }
+    return paths;
 }
