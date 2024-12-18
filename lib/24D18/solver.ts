c1c800d85b77d3ce83a3299d913d207461aa2e30
mabenj <markobenj@hotmail.com>
2024-12-18 23:34:53 +0200

    Refactor part 1 and 2 to use bfs

--- lib/24D18/solver.ts
+++ lib/24D18/solver.ts
@@ -1,12 +1,12 @@
 import { PuzzleSolver } from "../types/puzzle-solver.ts";
 
-const WIDTH = 70 + 1;
-const HEIGHT = 70 + 1;
+const WIDTH = 70;
+const HEIGHT = 70;
 const OBSTACLE_COUNT = 1024;
 const START: Node = `${0},${0}`;
-const END: Node = `${WIDTH - 1},${HEIGHT - 1}`;
+const END: Node = `${WIDTH},${HEIGHT}`;
 
-type Graph = Map<Node, Map<Node, number>>;
+type Graph = Map<Node, Node[]>;
 type Node = `${number},${number}`;
 
 export default class Solver24D18 implements PuzzleSolver {
@@ -23,7 +23,7 @@ export default class Solver24D18 implements PuzzleSolver {
 
     solvePart1() {
         const graph = buildGraph(this.obstacles.slice(0, OBSTACLE_COUNT));
-        const distance = dijkstra(graph, START, END);
+        const distance = bfs(graph, START, END);
         return distance;
     }
 
@@ -34,7 +34,7 @@ export default class Solver24D18 implements PuzzleSolver {
         while (leftIdx <= rightIdx) {
             const midIdx = Math.floor(leftIdx + (rightIdx - leftIdx) / 2);
             const graph = buildGraph(this.obstacles.slice(0, midIdx + 1));
-            const distance = dijkstra(graph, START, END);
+            const distance = bfs(graph, START, END);
             if (distance === Infinity) {
                 boundary = this.obstacles[midIdx];
                 rightIdx = midIdx - 1;
@@ -46,59 +46,55 @@ export default class Solver24D18 implements PuzzleSolver {
     }
 }
 
-function dijkstra(graph: Graph, start: Node, end: Node) {
-    const distances = new Map<Node, number>();
-    graph.keys().forEach((node) => distances.set(node, Infinity));
-    distances.set(start, 0);
-    const pq: { node: Node; distance: number }[] = [];
-    pq.push({ node: start, distance: 0 });
+function bfs(graph: Graph, start: Node, end: Node) {
+    const visited = new Set([start]);
+    const queue: { node: Node; distance: number }[] = [
+        { node: start, distance: 0 },
+    ];
 
-    while (pq.length > 0) {
-        pq.sort((a, b) => a.distance - b.distance);
-        const u = pq.shift()!;
-        if (u.node === end) {
-            return u.distance;
+    while (queue.length > 0) {
+        const { node, distance } = queue.shift()!;
+        if (node === end) {
+            return distance;
         }
-
-        const neighbors = graph.get(u.node)!.entries();
-        for (const [v, distance] of neighbors) {
-            const alt = distances.get(u.node)! + distance;
-            if (alt < distances.get(v)!) {
-                distances.set(v, alt);
-                pq.push({ node: v, distance: alt });
+        const neighbors = graph.get(node)!;
+        for (const neighbor of neighbors) {
+            if (visited.has(neighbor)) {
+                continue;
             }
+            visited.add(neighbor);
+            queue.push({ node: neighbor, distance: distance + 1 });
         }
     }
-
-    return distances.get(end)!;
+    return Infinity;
 }
 
 function buildGraph(obstacles: Node[]) {
     const obstacleSet = new Set(obstacles);
     const graph: Graph = new Map();
-    for (let x = 0; x < WIDTH; x++) {
-        for (let y = 0; y < HEIGHT; y++) {
+    for (let x = 0; x <= WIDTH; x++) {
+        for (let y = 0; y <= HEIGHT; y++) {
             const node: Node = `${x},${y}`;
             if (obstacleSet.has(node)) {
                 continue;
             }
-            graph.set(node, new Map());
+            graph.set(node, []);
 
             const north: Node = `${x},${y - 1}`;
             const south: Node = `${x},${y + 1}`;
             const west: Node = `${x - 1},${y}`;
             const east: Node = `${x + 1},${y}`;
             if (!obstacleSet.has(north) && y > 0) {
-                graph.get(node)!.set(north, 1);
+                graph.get(node)!.push(north);
             }
-            if (!obstacleSet.has(south) && y < HEIGHT - 1) {
-                graph.get(node)!.set(south, 1);
+            if (!obstacleSet.has(south) && y < HEIGHT) {
+                graph.get(node)!.push(south);
             }
             if (!obstacleSet.has(west) && x > 0) {
-                graph.get(node)!.set(west, 1);
+                graph.get(node)!.push(west);
             }
-            if (!obstacleSet.has(east) && x < WIDTH - 1) {
-                graph.get(node)!.set(east, 1);
+            if (!obstacleSet.has(east) && x < WIDTH) {
+                graph.get(node)!.push(east);
             }
         }
     }
