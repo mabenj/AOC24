eb0d4a1b1e3ebd743789b96daa88618e7732669b
mabenj <markobenj@hotmail.com>
2024-12-21 01:00:13 +0200

    Solve day 20 part 2

--- lib/24D20/solver.ts
+++ lib/24D20/solver.ts
@@ -1,140 +1,84 @@
 import { PuzzleSolver } from "../types/puzzle-solver.ts";
 
-const DIRECTIONS = [
-    [0, -1], // North
-    [1, 0], // East
-    [0, 1], // South
-    [-1, 0], // West
-];
-
-const EXAMPLE = `###############
-#...#...#.....#
-#.#.#.#.#.###.#
-#S#...#.#.#...#
-#######.#.#.###
-#######.#.#...#
-#######.#.###.#
-###..E#...#...#
-###.#######.###
-#...###...#...#
-#.#####.#.###.#
-#.#...#.#.#...#
-#.#.#.#.#.#.###
-#...#...#...###
-###############
-`;
-
 type Node = {
     id: string;
     x: number;
     y: number;
-    type: "#" | ".";
 };
-type AdjacencyList = Map<string, Node[]>;
+
 type Shortcut = {
     start: Node;
     end: Node;
-    size: number;
+    savedTime: number;
 };
 
 export default class Solver24D20 implements PuzzleSolver {
-    private adjacencyList: AdjacencyList = new Map();
     private track: Node[] = [];
 
     parseInput(input: string[]) {
-        // input = EXAMPLE.split("\n");
         const grid = input
             .filter((line) => !!line)
             .map((line) => line.split(""));
 
-        const getNode = (x: number, y: number): Node | null => {
-            const char = grid[y]?.[x];
-            if (!char) return null;
-            const id = `${x},${y}`;
-            return { id, x, y, type: char !== "#" ? "." : "#" };
-        };
+        let currentY = grid.findIndex((row) => row.includes("S"));
+        let currentX = grid[currentY].indexOf("S");
+        let prevX = -1;
+        let prevY = -1;
 
-        // Build adjacency list
-        let start: Node | undefined;
-        let end: Node | undefined;
-        for (let y = 0; y < grid.length; y++) {
-            for (let x = 0; x < grid[y].length; x++) {
-                const current = getNode(x, y)!;
-                const neighbors: Node[] = [];
-                for (const [dx, dy] of DIRECTIONS) {
-                    const neighbor = getNode(x + dx, y + dy);
-                    if (neighbor) neighbors.push(neighbor);
-                }
-                this.adjacencyList.set(current.id, neighbors);
+        const canMoveTo = (x: number, y: number) =>
+            grid[y][x] !== "#" && (x !== prevX || y !== prevY);
 
-                if (grid[y][x] === "S") start = current;
-                if (grid[y][x] === "E") end = current;
-            }
-        }
-
-        if (!start || !end) throw new Error("Start or end not found");
-
-        // Discover track
-        let prev: Node | null = null;
-        let current = start;
-        while (this.track.at(-1)?.id !== end.id) {
-            this.track.push(current);
-            const neighbors = this.adjacencyList.get(current.id)!;
-            const next = neighbors.find(
-                (node) => node.id !== prev?.id && node.type === "."
-            )!;
-            prev = current;
-            current = next;
+        while (prevX !== currentX || prevY !== currentY) {
+            this.track.push({
+                x: currentX,
+                y: currentY,
+                id: `${currentX},${currentY}`,
+            });
+            if (grid[currentY][currentX] === "E") break;
+            const canGoNorth = canMoveTo(currentX, currentY - 1);
+            const canGoEast = canMoveTo(currentX + 1, currentY);
+            const canGoSouth = canMoveTo(currentX, currentY + 1);
+            const canGoWest = canMoveTo(currentX - 1, currentY);
+            prevX = currentX;
+            prevY = currentY;
+            if (canGoNorth) currentY--;
+            if (canGoEast) currentX++;
+            if (canGoSouth) currentY++;
+            if (canGoWest) currentX--;
         }
     }
 
     solvePart1() {
-        const getShortcuts = (node: Node): Shortcut[] => {
-            const fullDistance = distanceToEnd(this.track, node.id);
-            const walls = this.adjacencyList
-                .get(node.id)!
-                .filter((neighbor) => neighbor.type === "#");
-            const shortcuts: { [endId: string]: Shortcut } = {};
-            for (const wall of walls) {
-                const validExits = this.adjacencyList
-                    .get(wall.id)!
-                    .filter(
-                        (neighbor) =>
-                            neighbor.id !== node.id && neighbor.type === "."
-                    );
-                const potentialShortcuts = validExits
-                    .map((exit) => ({
-                        start: node,
-                        end: exit,
-                        size: fullDistance - distanceToEnd(this.track, exit.id),
-                    }))
-                    .filter(({ size }) => size > 0);
-                const bestShortcut = potentialShortcuts.reduce(
-                    (best, current) => {
-                        return best.size > current.size ? best : current;
-                    },
-                    potentialShortcuts[0]
-                );
-                if (bestShortcut) shortcuts[bestShortcut.end.id] = bestShortcut;
-            }
-            return Object.values(shortcuts);
-        };
-
-        const allShortcuts = this.track.flatMap(getShortcuts);
-        return allShortcuts.filter(({ size }) => size - 2 >= 100).length;
+        const shortcuts = getShortcuts(this.track, 2);
+        return shortcuts.filter(({ savedTime }) => savedTime >= 100).length;
     }
 
-    solvePart2(): number | string {
-        throw new Error("Method not implemented.");
+    solvePart2() {
+        const shortcuts = getShortcuts(this.track, 20);
+        return shortcuts.filter(({ savedTime }) => savedTime >= 100).length;
     }
 }
 
-const distanceCache = new Map<string, number>();
-function distanceToEnd(track: Node[], nodeId: string) {
-    if (!distanceCache.has(nodeId)) {
-        const distance =
-            track.length - track.findIndex(({ id }) => id === nodeId);
-        distanceCache.set(nodeId, distance);
+function getShortcuts(track: Node[], shortCutDuration: number) {
+    // Manhattan distance
+    const distanceBetween = (a: Node, b: Node) =>
+        Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
+
+    const shortcuts: Shortcut[] = [];
+    for (let i = 0; i < track.length; i++) {
+        const startNode = track[i];
+        const fullDistance = track.length - i;
+        for (let j = i + 1; j < track.length; j++) {
+            const endNode = track[j];
+            const delta = distanceBetween(startNode, endNode);
+            if (delta > shortCutDuration) continue;
+            const shortcutDistance = delta + track.length - j;
+            shortcuts.push({
+                start: startNode,
+                end: endNode,
+                savedTime: fullDistance - shortcutDistance,
+            });
+        }
     }
-    return distanceCache.get(nodeId)!;
+    return shortcuts;
 }
