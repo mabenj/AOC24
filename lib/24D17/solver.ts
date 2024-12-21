571b1fba12b71480f0b7a51857561c482784222a
mabenj <markobenj@hotmail.com>
2024-12-21 23:36:05 +0200

    Solve day 17 part 2

--- lib/24D17/solver.ts
+++ lib/24D17/solver.ts
@@ -1,23 +1,10 @@
 import { PuzzleSolver } from "../types/puzzle-solver.ts";
 
-const EXAMPLE = `Register A: 729
-Register B: 0
-Register C: 0
-
-Program: 0,1,5,4,3,0
-`;
-// const EXAMPLE = `Register A: 2024
-// Register B: 0
-// Register C: 0
-
-// Program: 0,3,5,4,3,0`;
-
 export default class Solver24D17 implements PuzzleSolver {
     private registerA = 0;
     private program: number[] = [];
 
     parseInput(input: string[]) {
-        // input = EXAMPLE.split("\n");
         this.registerA = +input[0].split(": ")[1];
         this.program = input[4].split(": ")[1].split(",").map(Number);
     }
@@ -60,7 +47,51 @@ export default class Solver24D17 implements PuzzleSolver {
         return output.join(",");
     }
 
+    // Note: this only works for program: 2, 4, 1, 2, 7, 5, 4, 3, 0, 3, 1, 7, 5, 5, 3, 0
     solvePart2() {
-        return -1;
+        const PROGRAM = [2, 4, 1, 2, 7, 5, 4, 3, 0, 3, 1, 7, 5, 5, 3, 0];
+        //               B = A % 8
+        //                     B = B ^ 2
+        //                           C = A >> B
+        //                                 B = B ^ C
+        //                                       A = A >> 3
+        //                                             B = B ^ 7
+        //                                                    out(B % 8)
+        //                                                          if A != 0 jump to beginning
+
+        const find = (
+            program: bigint[],
+            answerSoFar: bigint
+        ): bigint | null => {
+            if (program.length === 0) {
+                return answerSoFar;
+            }
+            const target = program.at(-1);
+            const registers = { A: 0n, B: 0n, C: 0n };
+            for (const possibleB of [0n, 1n, 2n, 3n, 4n, 5n, 6n, 7n]) {
+                registers["A"] = (answerSoFar << 3n) + possibleB;
+
+                registers["B"] = possibleB; // this is same as B = A % 8
+                registers["B"] = registers["B"] ^ 2n;
+                registers["C"] = registers["A"] >> registers["B"];
+                registers["B"] = registers["B"] ^ registers["C"];
+                registers["B"] = registers["B"] ^ 7n;
+
+                const isPotentialSolution = registers["B"] % 8n === target;
+                if (isPotentialSolution) {
+                    const subAnswer = find(
+                        program.slice(0, program.length - 1),
+                        registers["A"]
+                    );
+                    if (subAnswer != null) {
+                        return subAnswer;
+                    }
+                }
+            }
+
+            return null;
+        };
+
+        return Number(find(PROGRAM.map(BigInt), 0n));
     }
 }
